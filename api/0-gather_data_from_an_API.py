4decff956fd8caf9052c7576478ea3d38f44a14f
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-02-24 07:22:53 +0000

    updates...

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,10 +10,17 @@ import requests
 
 if __name__ == "__main__":
 
-    """takes in an employee ID and returns information about his/her progress"""
     request_employee = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{argv[1]}/', timeout=10)
+
+    """
+        converts the json string to a python dictionary
+    """
     employee = json.loads(request_employee.text)
+
+    """
+        extracts the name of the employee
+    """
     employee_name = employee.get("name")
 
     request_todos = requests.get(
@@ -21,19 +28,19 @@ if __name__ == "__main__":
     tasks = {}
 
     employee_todos = json.loads(request_todos.text)
-
+    """ loops through the list of dictionaries """
+    # adds the title of the task and the status of the task to the dictionary
     for dictionary in employee_todos:
-        """ loops through the list of dictionaries """
-        # adds the title of the task and the status of the task to the dictionary
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
     EMPLOYEE_NAME = employee_name
     TOTAL_NUMBER_OF_TASKS = len(tasks)
     NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
 
+    """prints out the tasks that are completed"""
+    # loops through the dictionary and prints out the title of the task
     print(f"Employee {EMPLOYEE_NAME} is done with tasks \
         ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
     for k, v in tasks.items():
-        """prints out the tasks that are completed"""
         if v is True:
             print(f"\t {k}")
